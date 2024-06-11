8713541d81ae4e854d5257f8e50a29017af994dc
gallbotond <gallbotondofc@gmail.com>
2024-06-11 04:35:20 +0300

    clean up

--- Librewolf Wifi.ps1
+++
@@ -1,5 +0,0 @@
-Disable-NetAdapter -Name "Ethernet" -Confirm:$false
-& 'C:\Program Files (x86)\ForceBindIP\ForceBindIP64.exe' -i '{8048A100-02E2-40EF-A348-88FC14A70A20}' 'C:\Program Files\LibreWolf\librewolf.exe'
-Start-Sleep -Seconds 15
-Enable-NetAdapter -Name "Ethernet" -Confirm:$false
-exit
--- run-speedtest.ps1
+++
@@ -1,9 +0,0 @@
-# run speedtest
-$job = Start-Job -ScriptBlock {& 'C:\Program Files\ookla-speedtest-1.2.0-win64\speedtest.exe'}
-
-# wait for 2 seconds
-Start-Sleep -Seconds 5
-
-$result = Receive-Job -Job $job -Keep
-
-$result
