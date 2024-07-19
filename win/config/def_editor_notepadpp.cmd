05ccb5e50465ffc53e9f48df276f05e3bbd84739
Gáll Botond <gallbotondofc@gmail.com>
2024-07-19 12:01:31 +0300

    refactor

---
+++ win/config/def_editor_notepadpp.cmd
@@ -0,0 +1,1 @@
+REG ADD “HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe” /v “Debugger” /t REG_SZ /d “\”%ProgramFiles%\Notepad++\notepad++.exe\” -notepadStyleCmdline -z” /f
---
+++ win/config/reset_def_editor.cmd
@@ -0,0 +1,1 @@
+REG DELETE “HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe” /v “Debugger” /f
